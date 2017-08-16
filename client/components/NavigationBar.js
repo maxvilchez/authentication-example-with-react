582198466ed41607eb55f65144e2c7afd74e7a69
Max <max.vilchez@targetmaps.pe>
2017-08-16 09:46:07 -0500

    Authorization and validation

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from './../actions/authActions';
 
-const NavigationBar = () => (
-    <Menu fixed='top'>
-        <Link className="item header" to='/'>Our Company</Link>
-        <Menu.Menu position='right'>
-            <Link className="item" to='/register'>Sign up</Link>
-            <Link className="item" to='/login'>Login</Link>
-        </Menu.Menu>
-    </Menu>
-);
+class NavigationBar extends Component {
+    constructor(){
+        super();
+        this.logout = this.logout.bind(this);
+    }
+    logout() {
+        this.props.logout();
+    }
+    render() {
+        const { isAuthorization } = this.props.auth;
+        const userLinks = (
+            <Menu.Menu position='right'>
+                <Menu.Item onClick={this.logout}>Cerrar Sesión</Menu.Item>
+            </Menu.Menu>
+        );
+        const guestLinks = (
+            <Menu.Menu position='right'>
+                <Link className="item" to='/register'>Regístrate</Link>
+                <Link className="item" to='/login'>Iniciar Sesión</Link>
+            </Menu.Menu>
+        )
+        return (
+            <Menu fixed='top'>
+                <Link className="item header" to='/'>Our Company</Link>
+                { isAuthorization ?  userLinks : guestLinks }
+            </Menu>
+        );
+    }
+}
 
-export default NavigationBar;
+NavigationBar.proptypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth
+    }
+} 
+
+export default connect(mapStateToProps, {logout})(NavigationBar);
--- client/index.js
+++ client/index.js
@@ -7,6 +7,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import setAuthorizationToken from './utils/setAuthorizationToken';
 
+import jwt from 'jsonwebtoken';
+import { setCurrentUser } from './actions/authActions';
+
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './components/App';
@@ -19,7 +22,10 @@ const store = createStore(
     )
 );
 
-setAuthorizationToken(localStorage.jwtToken);
+if(localStorage.jwtToken){
+    setAuthorizationToken(localStorage.jwtToken);
+    store.dispatch(setCurrentUser(jwt.decode(localStorage.jwtToken)));
+}
 
 render(
     <Provider store={store}>
